d40fe721e902f4b2fea21274f2e2ec2c07355faf
Billy Somers <somersbilly@gmail.com>
2023-01-26 19:21:50 +1000

    Created about, layout, and other pages

---
+++ layouts/footer/Footer.tsx
@@ -0,0 +1,18 @@
+// Footer page for all pages
+
+// import React from 'react';
+// import Head from 'next/head';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="content">
+                    <p>© 2021 Billy Somers</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ pages/about/index.tsx
@@ -0,0 +1,21 @@
+// About page
+
+import Header from '../../layouts/header/Header';
+import Footer from '../../layouts/footer/Footer';
+
+export default function About() {
+    return (
+        <>
+        <Header/>
+        
+        <div className="container">
+        <div className="content">
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </div>
+        </div>
+        <Footer/>
+        </>
+    )
+    }
+    
